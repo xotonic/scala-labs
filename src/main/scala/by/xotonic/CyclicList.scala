81195809268fac52f549a8e8615afb26aa55a52f
Дмитрий Кузьмин <xotonic@yandex.ru>
2017-09-21 02:00:16 +0700

    Some skeleton (very some)

---
+++ src/main/scala/by/xotonic/CyclicList.scala
@@ -0,0 +1,16 @@
+package by.xotonic
+
+/**
+  * Created by xoton on 21.09.2017.
+  */
+class CyclicList(first : Dog) {
+
+  private class Node (value : Dog, next: Node)
+
+  val root = first
+
+  def add (value : Dog) = {
+
+  }
+
+}
--- src/main/scala/by/xotonic/Hello.scala
+++ src/main/scala/by/xotonic/Hello.scala
@@ -1,9 +1,13 @@
 package by.xotonic
 
-object Hello extends Greeting with App {
-  println(greeting)
+
+object Hello extends App {
+
+  println("bar".take(1))
+
+  1.to(10) == (1 to 10)
+
+  val i = new CyclicList(Dog("booldog"));
+
 }
 
-trait Greeting {
-  lazy val greeting: String = "hello"
-}
